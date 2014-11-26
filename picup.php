7c1793d9e02404aa2102e6f9cfeda3869365a575
johnpasquarello <john.luke.pasquarello@gmail.com>
2014-11-26 00:41:37 -0500

    secured picup.php

--- picup.php
+++ picup.php
@@ -5,6 +5,8 @@
 
 include "connectdb.php";
 include "session.php";
+include "security.php";
+
 if($_FILES['fileToUpload']['error'] == UPLOAD_ERR_NO_FILE){
 	header("location: picpost.php");
 }  
@@ -29,43 +31,48 @@ else{
 
 
 	if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $tfile)){
-		echo "Shared!";
-		//TODO something better
-	}
-	else{
-		echo $tfile;
+		//echo "Shared!";
+		//this was a tad premature
+		echo "file recieved<br />";
 	}
 
 	$abspath = "~/www/prinstagram/".$tfile;
 	$pubvar = 0;
 	if($_POST["gname"] == "Public"){
 		$pubvar = 1;
-	}  
-	if($stmt = $mysqli->prepare("INSERT INTO photo (poster, caption, pdate, lnge, lat, lname, is_pub, image) values (?, ?, NOW(), ?, ?, ?, ?, ?)")){
-		$stmt->bind_param("ssddsis", $_SESSION["username"], $_POST["caption"], $_POST["lon"], $_POST["lat"], $_POST["locname"], $pubvar, $tfile);
-		$stmt->execute();
-		$stmt->close();
 	}
-
-	if($pubvar == 0){
-		$pid = 0;
-		if($stmt = $mysqli->prepare("select max(pid) from photo where poster = ?")){
-			$stmt->bind_param("s", $_SESSION["username"]);
+	if(isCaptionValid($_POST["caption"]) && isCaptionValid($_POST["locname"])){
+		if($stmt = $mysqli->prepare("INSERT INTO photo (poster, caption, pdate, lnge, lat, lname, is_pub, image) values (?, ?, NOW(), ?, ?, ?, ?, ?)")){
+			$stmt->bind_param("ssddsis", $_SESSION["username"], $_POST["caption"], $_POST["lon"], $_POST["lat"], $_POST["locname"], $pubvar, $tfile);
 			$stmt->execute();
-			$stmt->bind_result($pid);
-			$stmt->fetch();
 			$stmt->close();
-		}  
-		if($stmt = $mysqli->prepare("INSERT INTO shared (pid, gname, ownername) values (?, ?, ?)")){
-			$stmt->bind_param("iss", $pid, $_POST["gname"], $_SESSION["username"]);
-			$stmt->execute();
 		}
-	}
 
-	echo '<br><br><img src = "'.$tfile.'" alt="Your Picture">';
-	echo '<form action = "picpost.php" method = "get">';
-	echo '<br><br><input type = "submit" value = "Back">';
-	echo '</form>'; 
+
+		if($pubvar == 0){
+			$pid = 0;
+			if($stmt = $mysqli->prepare("select max(pid) from photo where poster = ?")){
+				$stmt->bind_param("s", $_SESSION["username"]);
+				$stmt->execute();
+				$stmt->bind_result($pid);
+				$stmt->fetch();
+				$stmt->close();
+			}  
+			if($stmt = $mysqli->prepare("INSERT INTO shared (pid, gname, ownername) values (?, ?, ?)")){
+				$stmt->bind_param("iss", $pid, $_POST["gname"], $_SESSION["username"]);
+				$stmt->execute();
+			}
+		}
+
+		echo '<br><br><img src = "'.$tfile.'" alt="Your Picture">';
+		echo '<form action = "picpost.php" method = "get">';
+		echo '<br><br><input type = "submit" value = "Back">';
+		echo '</form>';
+	}
+	else{
+		echo "Loction and caption names must not be longer than 100 characters.";
+		echo "<br /><a href=\"picpost.php\"> Back </a>";
+	}
 }
 ?>
 </html>
