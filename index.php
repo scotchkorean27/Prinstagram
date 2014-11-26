b6cb860f35f5213a47863ab5de1b6e6ddc0832ce
johnpasquarello <john.luke.pasquarello@gmail.com>
2014-11-26 12:01:08 -0500

    added a link to viewtags whenthere are tags to see

--- index.php
+++ index.php
@@ -20,22 +20,20 @@ else{
 	echo "<a href=\"sendmess.php\">Send Qweet</a>&nbsp";
 	echo "<a href=\"viewmess.php\">Check Qweets</a><br/>";
 
-	echo "<p>";
 	$notcount = 0;
 	if($stmt = $mysqli->prepare("SELECT count(*) from tag where taggee = ? and tstatus = 0")){
 		$stmt->bind_param('s', $_SESSION["username"]);
 		$stmt->execute();
 		$stmt->bind_result($notcount);
 		$stmt->fetch();
-		if($notcount == 0){
-			echo "You have no tags awaiting approval";}
-		else if($notcount == 1){
-			echo "You have 1 tag awaiting approval";}
-		else{
-			echo " You have " . $notcount . " tags awaiting approval.";}
+		if($notcount == 1){
+			echo "<a href=\"viewtags.php\">You have 1 tag awaiting approval</a><br />";
+		}
+		else if($notcount > 1){
+			echo "<a href=\"viewtags.php\">You have " . $notcount . " tags awaiting approval.</a><br />";
+		}
 		$stmt->close();
 	}
-	echo "</p>";
 
 	loadPics();
 }
